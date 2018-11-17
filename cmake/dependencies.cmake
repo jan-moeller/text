1619f39977cc9e89d53a935e35a75b43dcdae758
janster <drag-on@gmx.de>
2018-11-17 01:58:42 +0100

    Use CMAKE_CURRENT_SOURCE_DIR instead of CMAKE_SOURCE_DIR
    CMAKE_SOURCE_DIR always points to the topmost directory. If boost::text is included in a cmake project via add_subdirectories, this is most likely not the right directory. Using CMAKE_CURRENT_SOURCE_DIR fixes this problem.

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -34,15 +34,15 @@ endif ()
 ###############################################################################
 # GoogleTest
 ###############################################################################
-add_subdirectory(${CMAKE_SOURCE_DIR}/googletest-release-1.8.0)
-target_include_directories(gtest      INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.8.0/googletest/include)
-target_include_directories(gtest_main INTERFACE ${CMAKE_HOME_DIRECTORY}/googletest-release-1.8.0/googletest/include)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/googletest-release-1.8.0)
+target_include_directories(gtest      INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/googletest-release-1.8.0/googletest/include)
+target_include_directories(gtest_main INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/googletest-release-1.8.0/googletest/include)
 
 
 ###############################################################################
 # Google Benchmark
 ###############################################################################
-add_subdirectory(${CMAKE_SOURCE_DIR}/benchmark-v1.2.0)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/benchmark-v1.2.0)
 
 
 ###############################################################################
